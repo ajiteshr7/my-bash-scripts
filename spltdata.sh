2d7728a64f1ab4e03f97843d5e7017ebbdf54cad
Ajitesh Rai <ajiteshr7@gmail.com>
2017-03-21 09:54:32 +0530

    Initial commit Added my bash scripts which creates a dataset of particular lenght from json.

---
+++ spltdata.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+## Program to make dataset,                               ##
+## it makes a file named data followed no. of data in it. ##
+
+# Provide json file followed by number of line you need
+
+if [ $# -eq 2 ]
+then
+    n=`expr $2 + 1`
+    fname=$1
+    head -$n $fname > tmp.json
+    tail -n +2 tmp.json > tmp2.json && mv tmp2.json tmp.json
+    sed -i '$ s/.$//' tmp.json
+    name="data"`expr $n - 1`
+    mv tmp.json $name.json
+else
+    echo 'Error: Invalid arguments!'
+    echo 'Usage: <filename> <no. of lines>'
+fi
